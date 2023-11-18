8e4b7e50b581d5d4b2f28dd00bfc724aa92851a0
Ryan Garofano <ryanjgarofano@gmail.com>
2023-11-18 12:10:30 -0800

    Add intial form skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import InputField from "./InputField";
 
 function App() {
   return (
-    <div className="w-full h-full flex justify-center items-center bg-gray-700">
-      <span className="p-4 bg-blue-700 rounded-full text-white shadow-md shadow-gray-800 animate-bounce">React + Tailwind</span>
+    <div>
+      <form className="w-full h-full flex justify-center items-center bg-white">
+        <InputField name="First Name" />
+        <InputField name="Last Name" />
+        <InputField name="Phone Number" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
