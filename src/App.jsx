72fa911f84c5fd2dd8dae8b1eb1147103c776c93
Ryan Garofano <ryanjgarofano@gmail.com>
2023-11-17 21:31:43 -0800

    Initial commit client code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="w-full h-full flex justify-center items-center bg-gray-700">
+      <span className="p-4 bg-blue-700 rounded-full text-white shadow-md shadow-gray-800 animate-bounce">React + Tailwind</span>
+    </div>
+  )
+}
+
+export default App
