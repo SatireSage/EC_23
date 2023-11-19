87a42511fa013dfaef5e8ecea882496a6397e5f3
SatireSage <sahajsingh2313@gmail.com>
2023-11-18 21:50:54 -0800

    Updated backend

--- backend/main.py
+++ backend/main.py
@@ -4,29 +4,54 @@ warnings.filterwarnings("ignore")
 from twilio.rest import Client
 from dotenv import load_dotenv
 import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-link = "https://satiresage.github.io/EC_23/location"
+link = "http://207.23.186.174:5173/location"
 
-dotenv_path = os.path.join(os.path.dirname(__file__), "token.env")
-load_dotenv(dotenv_path)
 
-account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-from_number = os.getenv("TWILIO_PHONE_NUMBER")
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
-if not all([account_sid, auth_token, from_number]):
-    raise ValueError(
-        "Twilio account SID, auth token, or phone number not set as environment variables."
-    )
 
-client = Client(account_sid, auth_token)
+@app.route("/process_phone_number", methods=["POST"])
+def process_phone_number():
+    try:
+        data = request.get_json()
+        phone_number = data["phone_number"]
 
-users = ["7789811091"]
-message_body = (
-    "Please proceed to the nearest evacuation center. For guidance visit {}".format(
-        link
-    )
-)
+        dotenv_path = os.path.join(os.path.dirname(__file__), "token.env")
+        load_dotenv(dotenv_path)
 
-for user in users:
-    message = client.messages.create(to=user, from_=from_number, body=message_body)
+        account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+        from_number = os.getenv("TWILIO_PHONE_NUMBER")
+
+        if not all([account_sid, auth_token, from_number]):
+            raise ValueError(
+                "Twilio account SID, auth token, or phone number not set as environment variables."
+            )
+
+        client = Client(account_sid, auth_token)
+
+        users = ["7789811091"]
+        message_body = "Please proceed to the nearest evacuation center. For guidance visit {}".format(
+            link
+        )
+
+        for user in users:
+            message = client.messages.create(
+                to=user, from_=from_number, body=message_body
+            )
+
+        # Perform some operation with the phone number (replace this with your logic)
+        result = f"Received phone number: {phone_number}"
+
+        return jsonify({"result": result})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
