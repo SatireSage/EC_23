738b7d439809901e5e61ebd2acca4dfb3d5dfaf5
SatireSage <sahajsingh2313@gmail.com>
2023-11-18 21:09:34 -0800

    Updated for github pages

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,32 @@
 import warnings
+
 warnings.filterwarnings("ignore")
 from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 
-link = "http://207.23.186.174:5173/location"
+link = "https://satiresage.github.io/EC_23/location"
 
-dotenv_path = os.path.join(os.path.dirname(__file__), 'token.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), "token.env")
 load_dotenv(dotenv_path)
 
-account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-from_number = os.getenv('TWILIO_PHONE_NUMBER')
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+from_number = os.getenv("TWILIO_PHONE_NUMBER")
 
 if not all([account_sid, auth_token, from_number]):
-    raise ValueError("Twilio account SID, auth token, or phone number not set as environment variables.")
+    raise ValueError(
+        "Twilio account SID, auth token, or phone number not set as environment variables."
+    )
 
 client = Client(account_sid, auth_token)
 
-users = ['7789811091']
-message_body = "Please proceed to the nearest evacuation center. For guidance visit {}".format(link)
+users = ["7789811091"]
+message_body = (
+    "Please proceed to the nearest evacuation center. For guidance visit {}".format(
+        link
+    )
+)
 
 for user in users:
-    message = client.messages.create(
-        to=user,
-        from_=from_number,
-        body=message_body
-    )
+    message = client.messages.create(to=user, from_=from_number, body=message_body)
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/EC_23/',
   plugins: [react()],
 })
