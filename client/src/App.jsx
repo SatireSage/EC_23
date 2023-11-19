041dd49ccb30f96bf7694b81ee9f6a4a6d5f3226
SatireSage <sahajsingh2313@gmail.com>
2023-11-18 22:46:26 -0800

    Launching to ghp3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from "./Pages/SignUp";
 import Location from "./Location";
 
 function App() {
   return (
-    <BrowserRouter basename={"/EC_23/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/EC_23/'}>
       <Routes>
         <Route path="/" element={<SignUp />} /> {/* SignUp route */}
         <Route path="/location" element={<Location />} /> {/* New page route */}
